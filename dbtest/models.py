7f68e96e53d97b0afe1cb6b060c5faf570be0ecc
Ayooluwa Tioluwanimi <ijidakinroayooluwa@gmail.com>
2024-05-10 01:17:05 +0100

    Add files via upload

---
+++ dbtest/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    subject = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
